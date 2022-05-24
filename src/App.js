466542d6ebca525735706715d0d1998f895c8e3f
Illia Kononenko <ilyanes88@gmail.com>
2022-05-25 00:44:37 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Profile from "./components/Profile/Profile";
 import user from "./data/user.json";
+
 import Statistics from "./components/Statistics/Statistics";
 import statisticalData from "./data/statistical-data.json";
+
 import FriendList from "./components/FriendList/FriendList";
 import friends from "./data/friends.json";
+
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import transactions from "./data/transactions.json";
 
