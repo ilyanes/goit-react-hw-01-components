79d1f037077bb25fa048f469f92a394b1c986a36
Illia Kononenko <ilyanes88@gmail.com>
2022-07-03 08:43:26 +0300

    destr

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -15,12 +15,12 @@ import PropTypes from "prop-types";
 export default function FriendList({ friends }) {
   return (
     <ul className="friend-list">
-      {friends.map((friend) => (
+      {friends.map(({ avatar, name, isOnline, id }) => (
         <FriendListItem
-          avatar={friend.avatar}
-          name={friend.name}
-          isOnline={friend.isOnline}
-          key={friend.id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+          key={id}
         />
       ))}
     </ul>
